5cd1a9bcaec63fd047e203e0d7e1ded5877a9a01
SophGits <sophie.jiffy@gmail.com>
2014-12-02 15:27:38 +0000

    Search images folder for images and push them into an array.

--- cardflip/script.js
+++ cardflip/script.js
@@ -1,16 +1,5 @@
 // NB To overcome CORS, run a server with python -m SimpleHTTPServer
 
-var deck = [
-  {
-    icon: ''
-  },
-  {
-
-  },
-  {
-
-  }
-]
 
 // $('#new-deck').onClick(createDeck());
 
@@ -26,21 +15,14 @@ function createDeck(){
 // </section>
 
 
-var fileExt = {};
-    fileExt[0]=".png";
-    fileExt[1]=".jpg";
-    fileExt[2]=".gif";
+var deck = []
 $.ajax({
-    //This will retrieve the contents of the folder if the folder is configured as 'browsable'
-    url: 'images/',
-    success: function (data) {
-      console.log(data);
-       $("#fileNames").html('<ul>');
-       //List all png or jpg or gif file names in the page
-       $(data).find('a:contains(" + fileExt[0] + "),a:contains(" + fileExt[1] + "),a:contains(" + fileExt[2] + ")').each(function () {
-           var filename = this.href.replace(window.location.host, "").replace("http:///", "");
-           $("#fileNames").append( '<li>'+filename+'</li>');
-       });
-       $("#fileNames").append('</ul>');
-     }
-  });
+  url: 'images/',
+  success: function (data) {
+    // console.log(data)
+    $(data).find('a:contains(".jpg"),a:contains(" + .png + ")').each(function () {
+      var imgName = this.text.toString();
+      deck.push(imgName);
+     });
+   }
+});
