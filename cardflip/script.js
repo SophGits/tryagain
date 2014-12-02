fc127a6bb27d1a9469e3342270b97f54ba285ffb
SophGits <sophie.jiffy@gmail.com>
2014-12-02 17:35:38 +0000

    Create functionality to allow only two cards to be facing at once.

--- cardflip/script.js
+++ cardflip/script.js
@@ -1,25 +1,40 @@
 // NB To overcome CORS, run a server with python -m SimpleHTTPServer
 $(document).ready(function(){
+
   var deck = []
-
-  $('button').click(function(){
-    // create deck
-    $(deck).each(function(){
-      console.log(this);
-      $('body').append('<section class="container"><div class="card"><div class="front">a</div><div class="back"><img src="images/'+this+'"/>b</div></div></section>')
-    })
-  });
-
+  // get images
   $.ajax({
     url: 'images/',
     success: function (data) {
-      // console.log(data)
       $(data).find('a:contains(".jpg"),a:contains(" + .png + ")').each(function () {
         var imgName = this.text.toString();
         deck.push(imgName);
        });
      }
   });
+  // start new game
+  $('#create-deck').click(function(){
+    //clear deck
+    $('#deck').html("");
+    // create deck
+    $(deck).each(function(){
+      $('#deck').append('<div class="container"><div class="card"><div class="front">Front</div><div class="back"><img src="images/'+this+'"/></div></div></div>')
+    })
+  });
+  // select a card
+  $('section').on("click", ".card", function(e){
+    e.preventDefault();
+    if($('.flipped').length >= 2){
+      $('.card').removeClass('flipped')
+      $(this).addClass('flipped');
+    } else {
+      $(this).addClass('flipped');
+    }
+  });
 
+  // no idea why this won't work
+  (function(){
+    $('#create-deck').trigger('click');
+  })();
 
 });
