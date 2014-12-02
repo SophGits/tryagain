d377869fc6560d6994b24594816d78e1f95bd1a3
SophGits <sophie.jiffy@gmail.com>
2014-12-02 16:07:28 +0000

    Create cards on click, with images on the backs.

--- cardflip/script.js
+++ cardflip/script.js
@@ -1,28 +1,25 @@
 // NB To overcome CORS, run a server with python -m SimpleHTTPServer
+$(document).ready(function(){
+  var deck = []
+
+  $('button').click(function(){
+    // create deck
+    $(deck).each(function(){
+      console.log(this);
+      $('body').append('<section class="container"><div class="card"><div class="front">a</div><div class="back"><img src="images/'+this+'"/>b</div></div></section>')
+    })
+  });
+
+  $.ajax({
+    url: 'images/',
+    success: function (data) {
+      // console.log(data)
+      $(data).find('a:contains(".jpg"),a:contains(" + .png + ")').each(function () {
+        var imgName = this.text.toString();
+        deck.push(imgName);
+       });
+     }
+  });
 
 
-// $('#new-deck').onClick(createDeck());
-
-function createDeck(){
-
-}
-
-// <section class="container">
-//   <div id="card">
-//     <div class="front">a</div>
-//     <div class="back">b</div>
-//   </div>
-// </section>
-
-
-var deck = []
-$.ajax({
-  url: 'images/',
-  success: function (data) {
-    // console.log(data)
-    $(data).find('a:contains(".jpg"),a:contains(" + .png + ")').each(function () {
-      var imgName = this.text.toString();
-      deck.push(imgName);
-     });
-   }
 });
